1e1ef4d572941c6d4417c96d1e74df9960b466a7
bing.xu.sh <bing.xu.sh@dianping.com>
2017-12-20 20:03:26 +0800

    修改manatee的爬虫

--- manatee-core/src/main/java/cn/edu/tongji/manatee/dtos/ResultDTO.java
+++ manatee-core/src/main/java/cn/edu/tongji/manatee/dtos/ResultDTO.java
@@ -1,12 +1,15 @@
 package cn.edu.tongji.manatee.dtos;
 
+import java.io.Serializable;
+
 /**
  * Created by xubing on 13:17 for manatee.
  * <p>
  * I love coding forever
  */
 
-public class ResultDTO {
+public class ResultDTO implements Serializable {
+
 
 
 
